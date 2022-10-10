57b4de8ac5d5fc33501075679d50287758e667ef
Taras Vlokh <69712928+trsvl@users.noreply.github.com>
2022-10-10 22:14:17 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import EditableTimer from "./components/EditableTimer";
 import "./components/App.scss";
 
 function App() {
-  const [sessionTime, setSessionTime] = useState(0.1 * 60);
-  const [breakTime, setBreakTime] = useState(0.2 * 60);
+  const [sessionTime, setSessionTime] = useState(20 * 60);
+  const [breakTime, setBreakTime] = useState(5 * 60);
   const [currentText, setCurrentText] = useState("Work paused");
   return (
     <>
